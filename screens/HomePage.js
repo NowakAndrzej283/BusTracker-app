a5ac8dedb6e3813b13bfc5ee06e381101f73c3fc
Andrzej Nowak <nowakandrzej283@gmail.com>
2026-03-14 21:19:05 +0100

    add animation to submit & modify the UI

--- screens/HomePage.js
+++ screens/HomePage.js
@@ -1,22 +1,84 @@
-import {View, Text, StyleSheet, TextInput, Pressable} from 'react-native';
+import {View, Text, StyleSheet, TextInput, Pressable, Animated} from 'react-native';
+import { useEffect, useRef, useState } from 'react';
+
+
 import Octicons from '@expo/vector-icons/Octicons';
 import SimpleLineIcons from '@expo/vector-icons/SimpleLineIcons';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 import { LinearGradient } from 'expo-linear-gradient';
+
 import Searchbar from '../components/Searchbar';
+import SubmitButton from '../components/SubmitButton';
 
 
-export default function HomePage(){
+export default function HomePage({navigation}){
+    const slideAnim = useRef(new Animated.Value(400)).current;
+    const [buttonsPressed, setButtonsPressed] = useState(false);
+    const [text, onChangeText] = useState('');
+    const [textSecond, onChangeTextSecond] = useState('');
+
+
+    function handleOnPressSubmit(){
+        navigation.navigate('MapPage');
+    }
+
+
+    useEffect(()=> {
+        const checkButtons = ()=> {
+            console.log('inside console.log dfad')
+            if(text?.length > 1 &&  textSecond?.length > 1){
+                console.log('the value has been updated');
+                setButtonsPressed(true);
+            }else{
+                console.log(text, textSecond);
+                console.log('not yet');
+            }
+        };
+
+        checkButtons();
+        return console.log('check once again');
+
+    }, [text, textSecond])
+
+    useEffect(()=> {
+        const showSubmit = () => {
+            Animated.timing(slideAnim, {
+                toValue: 80,
+                duration: 1000,
+                useNativeDriver: true
+            }).start();
+        };
+
+        if(buttonsPressed){
+            showSubmit();
+        }
+
+        return console.log('show submit fired');
+    }, [buttonsPressed])
+
+
     return (
-        
-            <View style={styles.rootContainer}>
-                <LinearGradient colors={['rgba(19, 102, 39, 0.8)', 'transparent']} style={{flex: 1}}>
-                    <View style={styles.icon}>
-                        <Octicons name="location" size={100} color="black" />
-                    </View>
-                    <Searchbar/>
-                    <Searchbar/>
-                </LinearGradient>
-            </View>
+        <View style={styles.rootContainer}>
+            <LinearGradient colors={['rgba(19, 102, 39, 0.8)', 'transparent']} style={{flex: 1}}>
+                <View style={styles.icon}>
+                    <Octicons name="location" size={100} color="black" />
+                </View>
+                <View style={styles.info}>
+                    <Text style={styles.infoText}>Warsaw Bus Schedule</Text>
+                </View>
+                <Searchbar title={'Set the starting point'} onValueChange={onChangeText}/>
+                <Searchbar title={'Set the destination'} onValueChange={onChangeTextSecond}/>
+                    <Animated.View
+                        style={{
+                            position: 'relative',
+                            bottom: 50,
+                            transform:[{translateY: slideAnim}]
+                        }}
+                    >
+                        <SubmitButton onPress={handleOnPressSubmit}/>
+                    </Animated.View>
+            </LinearGradient>
+        </View>
 
     );
 }
@@ -27,7 +89,16 @@ const styles = StyleSheet.create({
     },
     icon: {
         alignItems: 'center',
-        marginVertical: 50
+        marginTop: 50
     },
-
+    info: {
+        height: 50,
+        marginBottom: 20,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    infoText: {
+        fontSize: 20,
+        fontWeight: '600'
+    }
 })
