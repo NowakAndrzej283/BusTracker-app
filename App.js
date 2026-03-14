28505309bf4bb298fd947cbd99828d32e76bafcc
Andrzej Nowak <nowakandrzej283@gmail.com>
2026-03-14 12:52:07 +0100

    add App.js

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import HomePage from './screens/HomePage';
+import TimetablePage from './screens/TimetablePage';
+import MapPage from './screens/MapPage';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style='dark'/>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomePage}/>
+          <Stack.Screen name="Timetable" component={TimetablePage}/>
+          <Stack.Screen name="MapPage" component={MapPage}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+     
+    </>
   );
 }
 
