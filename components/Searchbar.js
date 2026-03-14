ff2211e414dc579cd0acf0001953fb66f6737f9d
Andrzej Nowak <nowakandrzej283@gmail.com>
2026-03-14 21:18:09 +0100

    modify searchbar

--- components/Searchbar.js
+++ components/Searchbar.js
@@ -1,20 +1,47 @@
 import {View, Text, StyleSheet, TextInput, Pressable} from 'react-native';
+import { useState, useEffect } from 'react';
+
 import Octicons from '@expo/vector-icons/Octicons';
 import SimpleLineIcons from '@expo/vector-icons/SimpleLineIcons';
 
-export default function Searchbar(){
+export default function Searchbar({title, onValueChange}){
+    const [text, onChangeText] = useState(text);
+    const [isCorrect, setIsCorrect] = useState(false);
+
+    // check if the textInput is filled
+    let textSize = text?.length > 1;
+
     return (
         <View style={styles.searchContainer}>
             <View style={styles.searchItem}>
                 <TextInput 
-                    placeholder='Choose the bus stop'
+                    placeholder={title}
                     style={styles.input}
+                    value={text}
+                    onChangeText={onChangeText}
                 />
             </View>
             <View style={styles.button}>
-                <Pressable>
+                { textSize &&
+                <Pressable 
+                    onPress={() => {
+                        if(text.length > 1){
+                            onChangeText(text);
+                            onValueChange(text);
+                            console.log('inside the onPress', text);
+                            setIsCorrect(true);
+                        }else{
+                            setIsCorrect(false);
+                        }
+                    }}
+                    style={({pressed}) => 
+                        pressed ? styles.pressed : null
+                    }    
+                    disabled={isCorrect}
+                >
                     <Octicons name="check" size={32} color="black" />
                 </Pressable>
+                }
             </View>
         </View>
     );
@@ -26,25 +53,40 @@ const styles = StyleSheet.create({
         height: 80,
         padding: 10,
         flexDirection: 'row',
-        margin: 5,
-        borderRadius: 8
+        margin: 10,
+        borderRadius: 8,
+        elevation: 10
     },
     searchItem: {
         width: '80%',
-        margin: 10
+        marginRight: 10,
+        marginTop: 10,
+        marginBottom: 10
     },
     input: {
         backgroundColor: 'white',
         borderWidth: 2,
         borderColor: 'black',
         borderRadius: 10,
-        padding: 10
+        padding: 5,
+        fontSize: 18,
+        fontWeight: '500'
     },
     button: {
         flex:1,
         backgroundColor: 'green',
         justifyContent: 'center',
         alignItems: 'center',
-        borderRadius: 8
+        borderRadius: 8,
+        marginVertical: 12
+    },
+    disabledButton: {
+        opacity: 0.1,
+    },  
+    normal: {
+        backgroundColor: 'white'
+    },
+    pressed: {
+        opacity: 0.2
     }
 })
