496e2cfaa015d2594a80e5084b226e382415be04
Andrzej Nowak <nowakandrzej283@gmail.com>
2026-03-15 12:56:32 +0100

    add TransportTypeBar

---
+++ components/TransportTypeBar.js
@@ -0,0 +1,16 @@
+import {View, Text, StyleSheet, FlatList} from 'react-native';
+
+
+export default function TransportTypeBar({navigation}){
+    return (
+        <View style={styles.rootContainer}>
+            
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    rootContainer: {
+        flex: 1,
+    }
+})
