c6e385623240cddeef86264a7afaa75586b5ed23
Andrzej Nowak <nowakandrzej283@gmail.com>
2026-03-16 17:38:20 +0100

    add transport bar

--- components/TransportTypeBar.js
+++ components/TransportTypeBar.js
@@ -1,10 +1,18 @@
-import {View, Text, StyleSheet, FlatList} from 'react-native';
+import {View, Text, StyleSheet, FlatList, Pressable} from 'react-native';
 
 
-export default function TransportTypeBar({navigation}){
+export default function TransportTypeBar({navigation, data}){
     return (
         <View style={styles.rootContainer}>
-            
+            <FlatList 
+                data={data}
+                keyExtractor={(item)=> item.route_id}
+                renderItem={({item})=> (
+                    <Text>
+                        Line number {item.route_short_name} - {item.route_long_name}
+                    </Text>
+                )}
+            />
         </View>
     );
 }
