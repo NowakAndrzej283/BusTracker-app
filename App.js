5c5aee130cc89db79bca1ccd794d34c612d8defc
Andrzej Nowak <nowakandrzej283@gmail.com>
2026-03-16 17:40:41 +0100

    update App.js & udpate the UI with bottom bar

--- App.js
+++ App.js
@@ -2,21 +2,43 @@ import { StyleSheet, Text, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { LinearGradient } from 'expo-linear-gradient';
+
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 import HomePage from './screens/HomePage';
 import TimetablePage from './screens/TimetablePage';
 import MapPage from './screens/MapPage';
+import { initDB } from './services/db';
+import { useEffect } from 'react';
 
 
-const Stack = createNativeStackNavigator();
+
+//const Stack = createNativeStackNavigator();
+const BottomTab = createBottomTabNavigator();
 
 export default function App() {
+
+  useEffect(()=> {
+    const handleInitDB = async()=> {
+      const data = await initDB();
+      console.log('to jest data; ', data)
+    };
+
+    handleInitDB();
+
+    return console.log('inside the useEffect');
+  },[])
+
+
   return (
     <>
       <StatusBar style='dark'/>
       <NavigationContainer>
-        <Stack.Navigator screenOptions={{
+        <BottomTab.Navigator screenOptions={{
           headerBackground: () => (
             <LinearGradient
               colors={['rgba(147, 214, 163, 0.8)', 'transparent']} style={{flex: 1}}
@@ -27,17 +49,18 @@ export default function App() {
           headerTintColor: '#000000',
           headerTitleAlign: 'center'
         }}
+          
         >
-          <Stack.Screen name="Home" component={HomePage}
-            options={{title: 'Route'}}
+          <BottomTab.Screen name="Home" component={HomePage}
+            options={{title: 'Route', tabBarIcon: ({color}) => (<MaterialCommunityIcons name="routes" size={24} color='black' />)}}
           />
-          <Stack.Screen 
+          <BottomTab.Screen 
             name="TimetablePage" 
             component={TimetablePage} 
-            options={{title: 'Schedule'}}
+            options={{title: 'Schedule', tabBarIcon: ({color}) => (<FontAwesome6 name="bus-simple" size={24} color="black" />)}}
           />
-          <Stack.Screen name="MapPage" component={MapPage} options={{title: "Schedule"}}/>
-        </Stack.Navigator>
+          <BottomTab.Screen name="MapPage" component={MapPage} options={{title: "Maps", tabBarIcon: ({color}) => (<FontAwesome name="map" size={24} color="black" />)}}/>
+        </BottomTab.Navigator>
       </NavigationContainer>
      
     </>
--- screens/MapPage.js
+++ screens/MapPage.js
@@ -1,17 +1,32 @@
 import {View, Text, StyleSheet} from 'react-native';
-
+import MapView, { PROVIDER_GOOGLE }from 'react-native-maps';
 
 export default function MapPage(){
     return (
         <View style={styles.rootContainer}>
-            <Text>Timetable Page</Text>
+            {/* <MapView
+                provider={ PROVIDER_GOOGLE }
+                key={'map'}
+                style={styles.container}
+                initialRegion={{
+                    latitude: 52.25,
+                    longitude: 21.23,
+                    latitudeDelta: 0.05,
+                    longitudeDelta: 0.05
+                }}
+            /> */}
+            
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     rootContainer: {
+        flex: 1
+    },
+    container: {
         flex: 1,
-        marginTop: 50,
+        width: '100%', 
+        height: '100%'
     }
 })
